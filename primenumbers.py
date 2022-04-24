df4147f8518a2b4437a069d0eec7d43e33d3f1be
ambroseotundo <ambroseotundo9@gmail.com>
2022-04-24 05:36:43 -0400

    prime number

---
+++ primenumbers.py
@@ -0,0 +1,10 @@
+def prime(num):
+    if num> 1:  
+        for n in range(2,num):  
+            if (num % n) == 0:  
+                return False
+        return True
+    else:
+        return False
+print(prime(64))
+print(prime(5))
